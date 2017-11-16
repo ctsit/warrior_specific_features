98bd65cca12572194c13d5695fcd89abf8909761
suryayalla <prasannakumar006@gmail.com>
2017-11-16 16:14:02 -0500

    add ExternalModule.php file

---
+++ ExternalModule.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file
+ * Provides ExternalModule class for Warrior Specific Features.
+ */
+
+namespace WarriorSpecificFeatures\ExternalModule;
+
+use ExternalModules\AbstractExternalModule;
+use ExternalModules\ExternalModules;
+
+/**
+ * ExternalModule class for Warrior Specific Features.
+ */
+class ExternalModule extends AbstractExternalModule {
+
+    /**
+     * @inheritdoc
+     */
+    function hook_every_page_top($project_id) {
+        include_once 'includes/default_subject_id.php';
+        
+        warrior_specific_features_default_subject_id($project_id);
+    }
+}
